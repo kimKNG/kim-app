8854d47bb50d10ffe7bebce2eb17e052520f175d
Kim Nguyen <kimberlymey.kn@googlemail.com>
2022-11-07 21:39:52 +0100

    CSS Styling

--- src/components/layout.js
+++ src/components/layout.js
@@ -25,7 +25,6 @@ const Layout = ({ children }) => {
 
   return (
     <>
-      <Header siteTitle={data.site.siteMetadata?.title || `Title`} />
       <div
         style={{
           margin: `0 auto`,
@@ -40,9 +39,9 @@ const Layout = ({ children }) => {
             fontSize: `var(--font-sm)`,
           }}
         >
-          © {new Date().getFullYear()} &middot; Built with
+          © {new Date().getFullYear()} &middot; Built by
           {` `}
-          <a href="https://www.gatsbyjs.com">Gatsby</a>
+          <a href="https://twitter.com/kimnuyen_">Kim</a>
         </footer>
       </div>
     </>
